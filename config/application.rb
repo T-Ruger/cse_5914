b5ac90532b858ae3f3474a51dccde6cf25b816d3
Tyler Ruger <ruger.3@osu.edu>
2020-01-16 18:22:09 -0500

    Recreate initial app

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module MovieOnRails
+module Cse5914
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
