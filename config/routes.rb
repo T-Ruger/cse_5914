4267a41728ebad4a7d608f6a642c7bb404c40e75
Tyler Ruger <ruger.3@osu.edu>
2020-01-23 12:13:59 -0500

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-
+  devise_for :users do
+  	get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
   root to: "static_page#home"
   get '/movies', to: "static_page#movies"
+  get '/about', to: "static_page#about"
 end
