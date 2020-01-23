b0c7471a3f3abb05037fb37d18c9b62617e530a2
Tyler Ruger <ruger.3@osu.edu>
2020-01-23 08:54:02 -0500

    Movie mockup page

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,4 @@
+//= require rails-wjs
+//= require rails-wjsbootstrap-sprockets
+//= require turbolinks
+//= require_tree
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "static_page#home"
+  get '/movies', to: "static_page#movies"
 end
