a51fde38c84d64392b7d9fb77208d17c492fdd59
Caleb Fischer <fischer.740@osu.edu>
2020-02-04 16:21:28 -0500

    updated message UI

--- app/assets/javascripts/channels/room_channel.js
+++ app/assets/javascripts/channels/room_channel.js
@@ -18,14 +18,14 @@ $(function() {
 		        var content = messageTemplate.children().clone(true, true);
 		        content.find('[data-role="user-avatar"]').attr('src', data.user_avatar_url);
 		        content.find('[data-role="message-text"]').text(data.message);
-		        content.find('[data-role="message-date"]').text(data.updated_at);
+		        //content.find('[data-role="message-date"]').text(data.updated_at);
 		        $element.append(content);
 		        $element.animate({ scrollTop: $element.prop("scrollHeight")}, 1000);
           }else{
           	var content = watsonMessageTemplate.children().clone(true, true);
 		        content.find('[data-role="user-avatar"]').attr('src', data.user_avatar_url);
 		        content.find('[data-role="message-text"]').text(data.message);
-		        content.find('[data-role="message-date"]').text(data.updated_at);
+		        //content.find('[data-role="message-date"]').text(data.updated_at);
 		        $element.append(content);
 		        $element.animate({ scrollTop: $element.prop("scrollHeight")}, 1000);
           }
