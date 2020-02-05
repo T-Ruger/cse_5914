a41fc63471640dea99a2d32a5e3ffb29b3faef1e
Caleb Fischer <fischer.740@osu.edu>
2020-02-05 12:44:35 -0500

    UI fixes

--- app/controllers/watson_message_controller.rb
+++
@@ -1,2 +0,0 @@
-class WatsonMessageController < ApplicationController
-end
--- app/models/watson_message.rb
+++
@@ -1,7 +0,0 @@
-class WatsonMessage < ApplicationRecord
-	belongs_to :room, inverse_of: :watson_messages
-	
-	def as_json(options)
-    super(options).merge(user_avatar_url: user.gravatar_url)
-  end
-end
