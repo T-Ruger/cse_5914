b212042b3c231bbf8dccb46ea3a94c61658c28e3
Brock <mastin23@osu.edu>
2020-04-09 15:29:14 -0400

    Added most of Jerry's styling to the pages. Changed bootstrap default text color to white

--- db/migrate/20200227211151_drop_viewings.rb
+++
@@ -1,5 +0,0 @@
-class DropViewings < ActiveRecord::Migration[5.2]
-  def change
-  	drop_table :viewings
-  end
-end
--- db/migrate/20200305034026_drop_viewing.rb
+++
@@ -1,5 +0,0 @@
-class DropViewing < ActiveRecord::Migration[5.2]
-  def change
-  	drop_table :viewings
-  end
-end
